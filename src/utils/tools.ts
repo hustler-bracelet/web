4542634f1e34232d1454a3ac7f5f2916680fb949
Ihor Belodedov <i.belodedov@rqc.ru>
2024-05-18 00:09:03 +0300

    countEmojis jsdoc

--- src/utils/tools.ts
+++ src/utils/tools.ts
@@ -1,4 +1,10 @@
-// Функция для подсчета эмоджи в строке
+/**
+ * Функция для подсчета эмоджи в строке
+ *
+ * @param {string} str - строка
+ * @param {RegExp} regex - регулярное выражние
+ * @returns {number} - кол-во эмоджи в строке
+ */
 export const countEmojis = (str: string, regex: RegExp): number => {
   return Array.from(str.matchAll(regex)).length;
 };
